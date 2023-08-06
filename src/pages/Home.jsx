66d3cc8d2bd6f6ea13e9eb7877c726e7764ad7db
Gaurav panwar <gp771234@gmail.com>
2023-08-07 01:23:16 +0530

    sun bana

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,10 @@ const Home = () => {
       <div id="earth"></div>
       <div id="mars"></div>
       <div id="jupiter"></div>
-      <div id="saturn"></div>
+      <div id="saturn">
+        <div className="planet"></div>
+        <div className="rings"></div>
+      </div>
       <div id="uranus"></div>
       <div id="neptune"></div>
     </main>
