49412390324a00f2ccfce2be6a347a38243ec1e3
Gaurav panwar <gp771234@gmail.com>
2023-08-07 01:06:10 +0530

    planets lagao

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import earth from "../assets/earth_map.jpg";
+
+const Home = () => {
+  return (
+    <main className="home">
+      <div id="venus"></div>
+      <div id="earth"></div>
+      <div id="mars"></div>
+      <div id="jupiter"></div>
+      <div id="saturn"></div>
+      <div id="uranus"></div>
+      <div id="neptune"></div>
+    </main>
+  );
+};
+
+export default Home;
